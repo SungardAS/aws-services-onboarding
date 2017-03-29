d3379ed878e8d76f39d131d3ffb91dd73fac7e9c
Alex Ough <alex.ough@sungardas.com>
2017-03-29 14:27:20 -0500

    don't add account in the final result

--- src/index_final_result.js
+++ src/index_final_result.js
@@ -1,6 +1,5 @@
 
 exports.handler = (event, context, callback) => {
-  event.final_result.account = event.account;
   event.final_result.credentials = event.health.credentials;
   event.final_result.alerts_destination = event.alerts_destination.result.body;
   event.final_result.health = event.health.result.body;
