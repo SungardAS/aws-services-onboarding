5e10afd235e8010e2ec5c4fbf93c5acf38dacd5e
Alex Ough <alex.ough@sungardas.com>
2017-04-21 00:51:47 -0500

    don't add an account role if the account is same with the master account

--- src/index_pre_new_account_federation.js
+++ src/index_pre_new_account_federation.js
@@ -1,7 +1,17 @@
 
 exports.handler = (event, context, callback) => {
   var roles = event.billing_master.roles;
-  roles.push({"roleArn": "arn:aws:iam::" + event.final_result.account_id + ":role/OrganizationAccountAccessRole"});
-  event.federation.roles = roles;
-  callback(null, event);
+
+  var role = new (require('aws-services-lib/aws/role'))();
+  role.findAccountId({}, function(err, accountId){
+    if (err)  callback(err);
+    else {
+      console.log("current account : " + accountId);
+      if (event.final_result.account_id != accountId) {
+        roles.push({"roleArn": "arn:aws:iam::" + event.final_result.account_id + ":role/OrganizationAccountAccessRole"});
+      }
+      event.federation.roles = roles;
+      callback(null, event);
+    }
+  });
 };
