32d26534093153b068c7a388b16323b762d972b4
Alex Ough <alex.ough@sungardas.com>
2017-05-08 20:09:21 -0500

    fixed the error when there is no alert destination in a region

--- src/index_alerts_destination.js
+++ src/index_alerts_destination.js
@@ -14,100 +14,97 @@ exports.handler = function (event, context) {
 
   console.log(JSON.stringify(event));
 
-  // find account list from the destination policy in the main region
-  findDestination(process.env.AWS_DEFAULT_REGION, event.destinationName, function(err, destination) {
-    if (err) {
-      console.log(err);
-      return context.fail(err, null);
+  var mainRegion = process.env.AWS_DEFAULT_REGION;
+
+  // find destination in the main region
+  var cloudwatchlogs = new AWS.CloudWatchLogs({region: mainRegion});
+  params = {
+    DestinationNamePrefix: event.destinationName
+  };
+  cloudwatchlogs.describeDestinations(params).promise().then(function(data) {
+    if (data && data.destinations.length > 0) {
+      console.log("found destination in main region, " + mainRegion + " : " + JSON.stringify(data));
+      if (data.destinations[0].accessPolicy) {
+        return data.destinations[0];
+      }
+      else {
+        var err = "no destination found in main region, " + mainRegion;
+        throw err;
+      }
     }
     else {
-      // add new account to the accessPolicyDocument
-      if (destination) {
-        var accessPolicyDocument = JSON.parse(destination.accessPolicy);
-        if (accessPolicyDocument.Statement[0].Principal.AWS.indexOf(event.accountToAdd) < 0) {
-          accessPolicyDocument.Statement[0].Principal.AWS.push(event.accountToAdd);
-        }
-        // find all regions and create/update the stack in each region
-        var ec2Main = new AWS.EC2({region:process.env.AWS_DEFAULT_REGION});
-        ec2Main.describeRegions({}, function(err, regions) {
-          if (err) {
-            return context.fail(err, null);
-          }
-          else {
-            build(regions.Regions, 0, event.destinationName, JSON.stringify(accessPolicyDocument), function(err, res) {
-              if (err) {
-                return context.fail(err, null);
-              }
-              else {
-                return context.done(null, createResponse(200, res));
-              }
-            });
-          }
+      var err = "no destination found in main region, " + mainRegion;
+      throw err;
+    }
+  }).then(function(destination) {
+    var accessPolicyDocument = JSON.parse(destination.accessPolicy);
+    if (accessPolicyDocument.Statement[0].Principal.AWS.indexOf(event.accountToAdd) < 0) {
+      accessPolicyDocument.Statement[0].Principal.AWS.push(event.accountToAdd);
+    }
+    destination.accessPolicy = JSON.stringify(accessPolicyDocument);
+    return destination;
+  }).then(function(destination) {
+    var promises = [];
+    var ec2Main = new AWS.EC2({region: mainRegion});
+    ec2Main.describeRegions({}).promise().then(function(data) {
+      data.Regions.forEach(function(region) {
+        promises.push(build(region.RegionName, destination));
+      });
+      var result = {};
+      Promise.all(promises).then(function(retArray, index) {
+        data.Regions.forEach(function(region) {
+          result[region.RegionName] = retArray[index];
         });
-      }
-      else {
-        var err = "no destination is found with name [" + event.destinationName + "]";
-        console.log(err);
-        return context.fail(err, null);
-      }
-    }
+        context.done(null, retArray);
+      }).catch(function(err) {
+        throw err;
+      });
+    }).catch(function(err) {
+      console.log("failed to find region list : " + err);
+      throw err;
+    });
+  }).catch(function(err) {
+    console.log(err);
+    return context.fail(err, null);
   });
 }
 
-function build(regions, idx, destinationName, accessPolicy, callback) {
-
-  var region = regions[idx].RegionName;
-
-  // check if this region already has the destination stack
+function build(region, destination) {
   var cloudwatchlogs = new AWS.CloudWatchLogs({region: region});
-  params = {
-    accessPolicy: accessPolicy,
-    destinationName: destinationName
+  var params = {
+    DestinationNamePrefix: destination.destinationName
   };
-  cloudwatchlogs.putDestinationPolicy(params, function(err, data) {
-    if (err) {
-      console.log('error in cloudwatchlogs.putDestinationPolicy : ' + err);
-      return callback(err, null);
+  cloudwatchlogs.describeDestinations(params).promise().then(function(data) {
+    if (data && data.destinations.length > 0) {
+      console.log("found destination in region, " + region + " : " + JSON.stringify(data));
+      return destination;
     }
     else {
+      console.log("no destination found in region, " + region + ", so create one");
+      params = {
+        destinationName: destination.destinationName,
+        roleArn: destination.roleArn,
+        targetArn: destination.targetArn
+      };
+      return cloudwatchlogs.putDestination(params).promise().then(function(data) {
+        console.log("completed to create a new destination in region, " + region);
+        return destination;
+      }).catch(function(err) {
+        console.log("failed to create a destination in region, " + region + ": " + err);
+        throw err;
+      });
+    }
+  }).then(function(destination) {
+    params = {
+      accessPolicy: destination.accessPolicy,
+      destinationName: destination.destinationName
+    };
+    cloudwatchlogs.putDestinationPolicy(params).promise().then(function(data) {
       console.log("completed to cloudwatchlogs.putDestinationPolicy in region, " + region);
-      result[region] = true;
-      if (++idx >= regions.length) {
-        return callback(null, result);
-      }
-      else {
-        build(regions, idx, destinationName, accessPolicy, callback);
-      }
-    }
-  });
-}
-
-function findDestination(region, destinationName, callback) {
-  var cloudwatchlogs = new AWS.CloudWatchLogs({region: region});
-  params = {
-    DestinationNamePrefix: destinationName
-  };
-  cloudwatchlogs.describeDestinations(params, function(err, data) {
-    if (err) {
-      console.log("failed to find destination in region, " + region + " : " + err);
-      callback(err, null);
-    }
-    else {
-      //console.log(data);
-      if (data && data.destinations.length > 0) {
-        console.log("found destination in region, " + region + " : " + JSON.stringify(data));
-        if (data.destinations[0].accessPolicy) {
-          //callback(null, data.destinations[0].accessPolicy);
-          callback(null, data.destinations[0]);
-        }
-        else {
-          callback(null, null);
-        }
-      }
-      else {
-        console.log("no destination found in region, " + region);
-        callback(null, null);
-      }
-    }
+      return true;
+    }).catch(function(err) {
+      console.log("failed to put a destination policy in region, " + region + ": " + err);
+      throw err;
+    });
   });
 }
