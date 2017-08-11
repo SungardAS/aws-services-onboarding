068d4c9a1157e40b56cfae649d74a84d4de01c9f
chandra.mishra <chandra.mishra@sungardas.com>
2017-08-11 19:49:37 +0530

    converted all codes into eslint style code

--- src/index_enable_billing_activation.js
+++ src/index_enable_billing_activation.js
@@ -1,92 +1,86 @@
 'use strict';
+
 const aws = require('aws-sdk');
-const req = require("request");
+
+const req = require('request');
+
 const fs = require('fs');
-const alertMailTemplate = "./template/alertmail.txt";
+
+const alertMailTemplate = './template/alertmail.txt';
 
 //----------------------------------------------------------------------
-var sendAlertMailBySes = function(params, errorMsg, from, to){
-  var ses = new aws.SES();
-  var emailParams = {
+function sendAlertMailBySes(params, errorMsg, billingData) {
+  const ses = new aws.SES();
+  const emailParams = {
     Destination: {
-      ToAddresses : to.split(',')
+      ToAddresses: billingData.billingToEmail.split(',')
     },
-    Source: from,
+    Source: billingData.billingFromEmail,
     Message: {
       Subject: {
         Data: 'MCAWS: Account Registration Failed with Billing System'
       },
-      Body: { Html: { Data: ''}}
+      Body: { Html: { Data: '' } }
     }
   };
-  fs.readFile(alertMailTemplate, function(err, data) {
-    if(err) throw err;
-    data = data.toString();
+  fs.readFile(alertMailTemplate, (err, data) => {
+    if (err) throw err;
+    let maildata = data.toString();
     // replacing predefine mail template with actual error message
-    data = data.replace(/__ERROR__/gm, JSON.stringify(errorMsg)); 
-    data = data.replace(/__PARAMS__/gm, JSON.stringify(params));
-    emailParams.Message.Body.Html.Data = data;
-    ses.sendEmail(emailParams, function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
-      else     console.log(data);           // successful response
+    maildata = maildata.replace(/__ERROR__/gm, JSON.stringify(errorMsg));
+    maildata = maildata.replace(/__PARAMS__/gm, JSON.stringify(params));
+    emailParams.Message.Body.Html.Data = maildata;
+    ses.sendEmail(emailParams, (error, res) => {
+      if (error) console.log(error, error.stack);
+      else console.log(res);
     });
   });
 }
 //----------------------------------------------------------------------
-var registerAccount = function(accId, params, event) {
-  var datetime = new Date();
-  var bodyjson = {
+function registerAccount(accId, params) {
+  const datetime = new Date();
+  const bodyjson = {
     sgId: params.SGID,
     offeringNum: params.OfferingNum,
     masterAwsId: params.masterAwsId,
     customerAwsId: accId,
     customerAwsName: params.name,
-    type: params.type ,
+    type: params.type,
     awsDesc: params.desc,
     activationDate: datetime
-  }
-  var billingUrl = params.billingUrl+"?client_id="+ params.apiKey +"&client_secret="+params.secretKey;
+  };
+  const billingUrl = `${params.billingUrl}?client_id=${params.apiKey}&client_secret=${params.secretKey}`;
   console.log(billingUrl);
   console.log(bodyjson);
-  try{
-    req({
-      url: billingUrl, //URL to hit
-      method: 'POST',
-      json: bodyjson 
-    },function(error, response, body){
-        console.log("error:" +JSON.stringify(error));
-        console.log("response:"+ JSON.stringify(response.body));
-        console.log("body:"+ JSON.stringify(body));
-        console.log("body:"+ JSON.stringify(response.statusCode));
-	if(error){
-          event.BillingActivationStatus=false;
-	  sendAlertMailBySes(bodyjson, error, params.billingFromEmail, params.billingToEmail)
-        }else{
-          if(response.body.errorcode == "0"){
-            event.BillingActivationStatus=true;
-          }else{
-            event.BillingActivationStatus=false;
-	    // send alrt mail to service now group
-	    sendAlertMailBySes(bodyjson, response.body, params.billingFromEmail, params.billingToEmail)
-	  }
-	}
-    });
-  }catch(err){
-    event.BillingActivationStatus=false;
-    sendAlertMailBySes(bodyjson, error, params.billingFromEmail, params.billingToEmail)
+  try {
+     req({url: billingUrl, method: 'POST',json: bodyjson },(error, response, body) => {
+        console.log(`error:  ${JSON.stringify(error)}`);
+        console.log(`response: ${JSON.stringify(response.body)}`);
+        console.log(`body: ${JSON.stringify(body)}`);
+        console.log(`body: ${JSON.stringify(response.statusCode)}`);
+        if (error) {
+          sendAlertMailBySes(bodyjson, error, params);
+        } else if (response.body.errorcode == '0') {
+          console.log('successfully registered with billing system');
+        } else {
+          sendAlertMailBySes(bodyjson, response.body, params);
+        }
+     });
+  } catch (err) {
+    sendAlertMailBySes(bodyjson, err, params);
   }
 }
 //----------------------------------------------------------------------
 exports.handler = (event, context, callback) => {
   // find account id
-  console.log(event)
-  var accountId = null;
-  var retDoc = event.account.result.body;
-  var billingInfo = event.account.billingDetails;
-  if (typeof(retDoc) == 'string') {
+  console.log(event);
+  let accountId = null;
+  let retDoc = event.account.result.body;
+  const billingInfo = event.account.billingDetails;
+  if (typeof retDoc === 'string') {
     retDoc = JSON.parse(retDoc);
   }
-  if(retDoc){ 
+  if (retDoc) {
     if (retDoc.Account) {
       // this is result of find account
       accountId = retDoc.Account.Id;
@@ -94,35 +88,19 @@ exports.handler = (event, context, callback) => {
       // this is result of create account
       accountId = retDoc.CreateAccountStatus.AccountId;
     }
-    if(accountId){
+    if (accountId) {
       // sending request to billing system(rest call) for register account
-      registerAccount(accountId, billingInfo, event);
-    } else{
-      console.log("customer account id not found:"+JSON.stringify(retDoc));// its happened when account creation failed
-      let errmsg = "Found Error While Creating Account:"+ JSON.stringify(retDoc.CreateAccountStatus.FailureReason);
-      sendAlertMailBySes(billingInfo, errmsg, billingInfo.billingFromEmail,billingInfo.billingToEmail)
-      event.BillingActivationStatus=false;
+      registerAccount(accountId, billingInfo);
+    } else {
+      console.log(`customer account id not found: ${JSON.stringify(retDoc)}`);
+      const errmsg = `Found Error While Creating Account:  ${JSON.stringify(retDoc.CreateAccountStatus.FailureReason)}`;
+      sendAlertMailBySes(billingInfo, errmsg, billingInfo);
     }
-  } else{
-      console.log("customer account id not found");
-      sendAlertMailBySes(billingInfo, "Found Error While Creating Account", billingInfo.billingFromEmail,billingInfo.billingToEmail)
-      event.BillingActivationStatus=false;
+  } else {
+    console.log('customer account id not found');
+    sendAlertMailBySes(billingInfo, 'Found Error While Creating Account');
   }
 
   callback(null, event);
 };
 //----------------------------------------------------------------------
-if (require.main === module) {
-  var  bodyjson= { sgId: '1111111111',
-    offeringNum: '123.2',
-    masterAwsId: '054649790173',
-    customerAwsId: '405723553155',
-    customerAwsName: 'Dino-1513',
-    type: 'Self-Managed',
-    awsDesc: 'Test MA-1513',
-    activationDate: "Tue Aug 01 2017 06:48:46 GMT+0000 (UTC)" 
-  }
-  sendAlertMailBySes(bodyjson, 'error', 'chandra.mishra@sungardas.com','chandra.mishra@sungardas.com,pratik.bhargava@sungardas.com,rajkumar.shivage@sungardas.com')
-}
-//----------------------------------------------------------------------
-
