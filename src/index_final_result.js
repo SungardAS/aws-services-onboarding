d84491597b2ccf5fff384563dc8c96b4fc9339ff
chandra.mishra <chandra.mishra@sungardas.com>
2018-01-11 19:04:20 +0530

    WIP

--- src/index_final_result.js
+++ src/index_final_result.js
@@ -1,9 +1,5 @@
 
 exports.handler = (event, context, callback) => {
-  console.log("---------------------------")
-  console.log(event)
-  console.log("---------------------------")
-
   event.final_result.credentials = event.health.credentials;
   //event.final_result.alerts_destination = event.alerts_destination.result.body;
   event.final_result.health = event.health.result.body;
