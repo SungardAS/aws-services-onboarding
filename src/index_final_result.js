e53baf8505c77cb5afa759f4036daa3bd48219f6
chandra.mishra <chandra.mishra@sungardas.com>
2018-01-11 18:10:08 +0530

    added debug

--- src/index_final_result.js
+++ src/index_final_result.js
@@ -1,7 +1,11 @@
 
 exports.handler = (event, context, callback) => {
+  console.log("---------------------------")
+  console.log(event)
+  console.log("---------------------------")
+
   event.final_result.credentials = event.health.credentials;
   event.final_result.alerts_destination = event.alerts_destination.result.body;
-  event.final_result.health = event.health.result.body;
+  //event.final_result.health = event.health.result.body;
   callback(null, event.final_result);
 };
--- src/index_set_results.js
+++ src/index_set_results.js
@@ -1,6 +1,9 @@
 
 exports.handler = (event, context, callback) => {
   // we've got all outputs of the parallel actions, so merge results to the first action's output
+  console.log("---------------------------")
+  console.log(event)
+  console.log("---------------------------")
   var cloudtrailResult = JSON.parse(event[0].cloudtrail.result.body);
   event[0].final_result.cloudtrail[event[0].cloudtrail.body.region] = cloudtrailResult.result;
   var awsconfigResult = JSON.parse(event[1].awsconfig.result.body);
