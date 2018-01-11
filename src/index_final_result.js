6650f4aee69506d1c96b2ea8c8761e5b6dddba2e
chandra.mishra <chandra.mishra@sungardas.com>
2018-01-11 19:25:12 +0530

    WIP

--- src/index_final_result.js
+++ src/index_final_result.js
@@ -1,7 +1,9 @@
 
 exports.handler = (event, context, callback) => {
   event.final_result.credentials = event.health.credentials;
-  //event.final_result.alerts_destination = event.alerts_destination.result.body;
+  if(event.alerts_destination.result){
+    event.final_result.alerts_destination = event.alerts_destination.result.body;
+  }
   event.final_result.health = event.health.result.body;
   callback(null, event.final_result);
 };
