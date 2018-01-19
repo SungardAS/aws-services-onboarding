87d71bd1d397f54efd869abe11243f898a221f1d
chandra.mishra <chandra.mishra@sungardas.com>
2018-01-19 17:23:27 +0530

    WIP

--- src/index_final_result.js
+++ src/index_final_result.js
@@ -4,6 +4,8 @@ exports.handler = (event, context, callback) => {
   if(event.alerts_destination.result){
     event.final_result.alerts_destination = event.alerts_destination.result.body;
   }
-  event.final_result.health = event.health.result.body;
+  if(event.health){
+    event.final_result.health = event.health.result.body;
+  }
   callback(null, event.final_result);
 };
--- src/index_set_results.js
+++ src/index_set_results.js
@@ -1,9 +1,13 @@
 
 exports.handler = (event, context, callback) => {
   // we've got all outputs of the parallel actions, so merge results to the first action's output
-  var cloudtrailResult = JSON.parse(event[0].cloudtrail.result.body);
-  event[0].final_result.cloudtrail[event[0].cloudtrail.body.region] = cloudtrailResult.result;
-  var awsconfigResult = JSON.parse(event[1].awsconfig.result.body);
-  event[0].final_result.awsconfig[event[0].awsconfig.body.region] = awsconfigResult.result;
+  if(event[0].cloudtrail){
+    var cloudtrailResult = JSON.parse(event[0].cloudtrail.result.body);
+    event[0].final_result.cloudtrail[event[0].cloudtrail.body.region] = cloudtrailResult.result;
+  }
+  if(event[1].awsconfig){
+    var awsconfigResult = JSON.parse(event[1].awsconfig.result.body);
+    event[0].final_result.awsconfig[event[0].awsconfig.body.region] = awsconfigResult.result;
+  }
   callback(null, event[0]);
 };
