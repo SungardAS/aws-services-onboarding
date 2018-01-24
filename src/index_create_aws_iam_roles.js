e8a311dde0e2bf9663c2addd004f0024b89778ab
chandra.mishra <chandra.mishra@sungardas.com>
2018-01-24 18:48:08 +0530

    WIP

--- src/index_create_aws_iam_roles.js
+++ src/index_create_aws_iam_roles.js
@@ -1,8 +1,29 @@
 
 const awsIamRole = require('lib/awsIamRole.js');
 
+
 exports.handler = function (event, context) {
 
   console.log(JSON.stringify(event));
-
+  var options = {
+    accessKeyId: event.credentials.Credentials.AccessKeyId,
+    secretAccessKey: event.credentials.Credentials.SecretAccessKey,
+    sessionToken: event.credentials.Credentials.SessionToken
+  };
+  var options.accountId= event.final_result.account_id;
+  var type = event.billingDetails.type;
+  console.log(options)
+  var awsroles = JSON.parse(fs.readFileSync(__dirname + '/json/default_roles.json', {encoding:'utf8'}));
+  var dataDogPolicyDoc = JSON.parse(fs.readFileSync(__dirname + '/json/datadog-integration_policy.json', {encoding:'utf8'}));
+  if(event.billingDetails && event.billingDetails.type){
+    roles = awsroles[type];
+    console.log(roles)
+    for (const role in roles) {
+      Object.assign(options, role);
+    console.log(options)
+      awsIamRole.createRole(options, function(err, data) {
+      }
+    }
+  }
+ callback(null, event);
 }
