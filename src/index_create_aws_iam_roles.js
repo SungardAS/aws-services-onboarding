74ffa06e9f08a51d20ea5303dc15828359307620
chandra.mishra <chandra.mishra@sungardas.com>
2018-02-07 17:11:25 +0530

    WIP

--- src/index_create_aws_iam_roles.js
+++ src/index_create_aws_iam_roles.js
@@ -1,52 +1,65 @@
-var fs = require('fs');
-var uuid = require('node-uuid');
+const fs = require('fs');
+const uuid = require('node-uuid');
 const awsIamRole = require('./lib/awsIamRole.js');
 
-
-exports.handler = function (event, context, callback) {
-
+exports.handler = function(event, context, callback) {
   console.log(JSON.stringify(event));
-  var options = {
+  const options = {
     accessKeyId: event.credentials.Credentials.AccessKeyId,
     secretAccessKey: event.credentials.Credentials.SecretAccessKey,
     sessionToken: event.credentials.Credentials.SessionToken
   };
-  var type = event.account.billingDetails.type;
-  var awsroles = JSON.parse(fs.readFileSync(__dirname + '/json/federate-role_info.json', {encoding:'utf8'}));
-  var dataDogPolicyDoc = JSON.parse(fs.readFileSync(__dirname + '/json/datadog-integration_policy.json', {encoding:'utf8'}));
+  const type = event.account.billingDetails.type;
+  const awsroles = JSON.parse(
+    fs.readFileSync(`${__dirname}/json/federate-role_info.json`, {
+      encoding: 'utf8'
+    })
+  );
+  const dataDogPolicyDoc = JSON.parse(
+    fs.readFileSync(`${__dirname}/json/datadog-integration_policy.json`, {
+      encoding: 'utf8'
+    })
+  );
 
-  options.account= event.final_result.account_id;
+  options.account = event.final_result.account_id;
   options.assumeRolePolicyDocument = awsroles.assumeRolePolicyDocument;
-  options.assumeRolePolicyDocument.Statement[0].Principal.AWS = "arn:aws:iam::442294194136:role/federate"
+  options.assumeRolePolicyDocument.Statement[0].Principal.AWS =
+    'arn:aws:iam::442294194136:role/federate';
   options.onboardAccount = true;
   options.path = awsroles.adminRolePath;
 
-  if(event.account.billingDetails && event.account.billingDetails.type){
-    var dbIamRoles = [];
-    roles = awsroles.roles[type.toLowerCase()];
-    //var dbAwsAccount = {accountId:options.account,accountName:event.account.billingDetails.name,accountDescription:event.account.billingDetails.desc,email:event.account.billingDetails.email,guid:event.account.billingDetails.guid,accountType:event.account.billingDetails.type};
-    for(i=0; i< roles.length; i++){
-      var payload = {};
+  if (event.account.billingDetails && event.account.billingDetails.type) {
+    const dbIamRoles = [];
+    const roles = awsroles.roles[type.toLowerCase()];
+    // var dbAwsAccount = {accountId:options.account,accountName:event.account.billingDetails.name,accountDescription:event.account.billingDetails.desc,email:event.account.billingDetails.email,guid:event.account.billingDetails.guid,accountType:event.account.billingDetails.type};
+    for (let i = 0; i < roles.length; i++) {
+      let payload = {};
       Object.assign(payload, options, roles[i]);
       payload.externalId = uuid.v4();
-      payload=JSON.parse(JSON.stringify(payload));
-      if(payload.roleName == 'DatadogAWSIntegrationRole'){
+      payload = JSON.parse(JSON.stringify(payload));
+      if (payload.roleName == 'DatadogAWSIntegrationRole') {
         payload.PolicyDocument = dataDogPolicyDoc;
-        payload.assumeRolePolicyDocument.Statement[0].Principal.AWS="arn:aws:iam::"+event.account.billingDetails.datadogAwsId+":root";
+        payload.assumeRolePolicyDocument.Statement[0].Principal.AWS = `arn:aws:iam::${event
+          .account.billingDetails.datadogAwsId}:root`;
       }
-      if(payload.federate){
-        dbIamRoles.push({account:payload.account,externalId:payload.externalId,path:payload.path,roleName:payload.roleName})
+      if (payload.federate) {
+        dbIamRoles.push({
+          account: payload.account,
+          externalId: payload.externalId,
+          path: payload.path,
+          roleName: payload.roleName
+        });
       }
-      awsIamRole.createRole(payload, function(err, data) {
-        console.log(err)
-        console.log(data)
-      })
+      awsIamRole.createRole(payload, (err, data) => {
+        console.log(err);
+        console.log(data);
+      });
     }
     event.dbIamRoles = dbIamRoles;
-    //event.dbAwsAccount = dbAwsAccount;
-  }else{
-    console.log("invalid account type")
-    console.log(type)
+    // event.dbAwsAccount = dbAwsAccount;
+  } else {
+    console.log('invalid account type');
+    console.log(type);
   }
- callback(null, event);
-}
+  callback(null, event);
+};
