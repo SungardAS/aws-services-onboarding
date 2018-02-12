f49fc582f95b1736ceab9b7c7b1e6a216966559a
chandra.mishra <chandra.mishra@sungardas.com>
2018-02-12 23:28:09 +0530

    WIP

--- src/index.js
+++ src/index.js
@@ -84,6 +84,7 @@ baseHandler.post = function(params, callback) {
   const encryptedBuf = new Buffer(process.env.DB_PASSWORD, 'base64');
   const cipherText = { CiphertextBlob: encryptedBuf };
   const kms = new AWS.KMS({region:process.env.KMS_REGION});
+  cosnole.log("==============");
   Promise.all(ruleJson.map(getRulesPayload)).then(function(configrules){
   cosnole.log(configrules);
   inputDoc.configrules.rules = configrules;
