cebf8cb125de6be187be50824c1eda7fefca700c
chandra.mishra <chandra.mishra@sungardas.com>
2018-02-12 12:36:45 +0530

    WIP

--- src/index_update_db.js
+++ src/index_update_db.js
@@ -1,34 +1,44 @@
+'use strict';
 const mysql = require('mysql');
+const AWS = require('aws-sdk');
 
 exports.handler = function(event, context, callback) {
   console.log(JSON.stringify(event));
   const dbIamRoles = event.dbIamRoles;
   const dbAwsAccount = event.dbAwsAccount;
-
-  const con = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER_NAME,
-    password: process.env.DB_PASSWORD,
-    database: 'msaws'
-  });
-  con.connect(err => {
-    if (err) throw err;
-    console.log('Connected!');
-    const sql1 = 'insert into awsaccount SET ?';
-    con.query(sql1, dbAwsAccount, (accErr, accData) => {
-      if (accErr) throw accErr;
-      if (dbAwsAccount.account_type != 'craws') {
-        for (let idx = 0; idx < dbIamRoles.length; idx++) {
-          dbIamRoles[idx].account = accData.insertId;
-          const sql2 = 'insert into awsiamrole SET ?';
-          con.query(sql2, dbIamRoles[idx], (roleErr, roleData) => {
-            console.log(roleErr);
-            console.log(roleData);
-          });
-        }
-      }
+  const encryptedBuf = new Buffer(process.env.DB_PASSWORD, 'base64');
+  const cipherText = { CiphertextBlob: encryptedBuf };
+  const kms = new AWS.KMS({ region: process.env.KMS_REGION });
+  kms.decrypt(cipherText, (err, passwd) => {
+    if (err) {
+      console.log('Decrypt error:', err);
+      return callback(err);
+    }
+    const con = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER_NAME,
+      password: passwd,
+      database: 'msaws'
     });
+    con.connect(dberr => {
+      if (dberr) throw dberr;
+      console.log('Connected!');
+      const sql1 = 'insert into awsaccount SET ?';
+      con.query(sql1, dbAwsAccount, (accErr, accData) => {
+        if (accErr) throw accErr;
+        if (dbAwsAccount.account_type != 'craws') {
+          for (let idx = 0; idx < dbIamRoles.length; idx++) {
+            dbIamRoles[idx].account = accData.insertId;
+            const sql2 = 'insert into awsiamrole SET ?';
+            con.query(sql2, dbIamRoles[idx], (roleErr, roleData) => {
+              console.log(roleErr);
+              console.log(roleData);
+            });
+          }
+        }
+      });
+    });
+    if (con) con.end();
   });
-  if (con) con.end();
   callback(null, event);
 };
