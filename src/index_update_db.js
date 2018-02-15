f0b1fccab4cb58e4eb4349c4e956c365158c6fd2
chandra.mishra <chandra.mishra@sungardas.com>
2018-02-15 22:24:28 +0530

    WIP

--- src/index_update_db.js
+++ src/index_update_db.js
@@ -1,6 +1,7 @@
 'use strict';
-const mysql = require('mysql');
+//const mysql = require('mysql');
 const AWS = require('aws-sdk');
+const mcawsModels = require('./models/mcawsModels.js');
 
 exports.handler = function(event, context, callback) {
   console.log(JSON.stringify(event));
@@ -14,31 +15,36 @@ exports.handler = function(event, context, callback) {
       console.log('Decrypt error:', err);
       return callback(err);
     }
-    const con = mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USERNAME,
-      password: passwd.Plaintext.toString('ascii'),
-      database: 'msaws'
-    });
-    con.connect(dberr => {
-      if (dberr) throw dberr;
-      console.log('Connected!');
-      const sql1 = 'insert into awsaccount SET ?';
-      con.query(sql1, dbAwsAccount, (accErr, accData) => {
-        if (accErr) throw accErr;
+    //const con = mysql.createConnection({
+    //  host: process.env.DB_HOST,
+    //  user: process.env.DB_USERNAME,
+    //  password: passwd.Plaintext.toString('ascii'),
+    //  database: 'msaws'
+    //});
+    //con.connect(dberr => {
+      mcawsModels.AwsAccount(process.env.DB_USERNAME,passwd.Plaintext.toString('ascii'),process.env.DB_HOST,'msaws', function(resp1) {
+      resp1.create(dbAwsAccount)
+      //if (dberr) throw dberr;
+      //console.log('Connected!');
+      //const sql1 = 'insert into awsaccount SET ?';
+      //con.query(sql1, dbAwsAccount, (accErr, accData) => {
+        //if (accErr) throw accErr;
         if (dbAwsAccount.account_type != 'craws') {
           for (let idx = 0; idx < dbIamRoles.length; idx++) {
             dbIamRoles[idx].account = accData.insertId;
-            const sql2 = 'insert into awsiamrole SET ?';
-            con.query(sql2, dbIamRoles[idx], (roleErr, roleData) => {
-              console.log(roleErr);
-              console.log(roleData);
-            });
-          }
-         if (con) con.end();
+            //const sql2 = 'insert into awsiamrole SET ?';
+            //con.query(sql2, dbIamRoles[idx], (roleErr, roleData) => {
+            //  console.log(roleErr);
+            //  console.log(roleData);
+            //});
+             mcawsModels.AwsIamRole(process.env.DB_USERNAME,passwd.Plaintext.toString('ascii'),process.env.DB_HOST,'msaws', function(resp2) {
+             resp2.create(roleData)
+          })
+         //if (con) con.end();
+        }
         }
       });
     });
-  });
+  //});
   callback(null, event);
 };
