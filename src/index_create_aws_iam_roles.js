8540385dbb3f7accfe567605a5de0cf70c6f7d8f
chandra.mishra <chandra.mishra@sungardas.com>
2018-02-16 13:47:27 +0530

    WIP

--- src/index_create_aws_iam_roles.js
+++ src/index_create_aws_iam_roles.js
@@ -47,28 +47,30 @@ exports.handler = function(event, context, callback) {
       company_guid: accountData.guid,
       account_type: accountData.type
     };
-    for (let i = 0; i < roles.length; i++) {
-      let payload = {};
-      Object.assign(payload, options, roles[i]);
-      payload.externalId = uuid.v4();
-      payload = JSON.parse(JSON.stringify(payload));
-      if (payload.roleName == 'DatadogAWSIntegrationRole') {
-        payload.PolicyDocument = dataDogPolicyDoc;
-        payload.assumeRolePolicyDocument.Statement[0].Principal.AWS = `arn:aws:iam::${process
-          .env.DATADOG_AWD_ID}:root`;
-      }
-      if (payload.federate) {
-        dbIamRoles.push({
-          externalId: payload.externalId,
-          path: payload.path,
-          name: payload.roleName,
-          arn: `arn:aws:iam::${payload.account}:role/${payload.roleName}`
+    if (dbAwsAccount.account_type != 'craws') {
+      for (let i = 0; i < roles.length; i++) {
+        let payload = {};
+        Object.assign(payload, options, roles[i]);
+        payload.externalId = uuid.v4();
+        payload = JSON.parse(JSON.stringify(payload));
+        if (payload.roleName == 'DatadogAWSIntegrationRole') {
+          payload.PolicyDocument = dataDogPolicyDoc;
+          payload.assumeRolePolicyDocument.Statement[0].Principal.AWS = `arn:aws:iam::${process
+            .env.DATADOG_AWD_ID}:root`;
+        }
+        if (payload.federate) {
+          dbIamRoles.push({
+            externalId: payload.externalId,
+            path: payload.path,
+            name: payload.roleName,
+            arn: `arn:aws:iam::${payload.account}:role/${payload.roleName}`
+          });
+        }
+        awsIamRole.createRole(payload, (err, data) => {
+          console.log('Error:', err);
+          console.log('Res:', data);
         });
       }
-      awsIamRole.createRole(payload, (err, data) => {
-        console.log('Error:', err);
-        console.log('Res:', data);
-      });
     }
     event.dbIamRoles = dbIamRoles;
     event.dbAwsAccount = dbAwsAccount;
