40659104b5a300c4048dc249cdbb1f1ea8bb101b
PratikB <ubuntu@ip-10-95-80-90.ec2.internal>
2018-05-23 14:11:28 +0000

    WIP

--- src/index_set_results.js
+++ src/index_set_results.js
@@ -1,11 +1,12 @@
 
 exports.handler = (event, context, callback) => {
+console.log(JSON.stringify(event));
   // we've got all outputs of the parallel actions, so merge results to the first action's output
   var cloudtrailResult = JSON.parse(event[0].cloudtrail.result.body);
   event[0].final_result.cloudtrail[event[0].cloudtrail.body.region] = cloudtrailResult.result;
   var awsconfigResult = JSON.parse(event[1].awsconfig.result.body);
   event[0].final_result.awsconfig[event[0].awsconfig.body.region] = awsconfigResult.result;
-  var awseventsResult = JSON.parse(event[2].awsevents.result.body);
-  event[0].final_result.awsevents[event[0].awsevents.region] = awseventsResult.result;
+//  var awseventsResult = JSON.parse(event[2].awsevents.result.body);
+//  event[0].final_result.awsevents[event[0].awsevents.region] = awseventsResult.result;
   callback(null, event[0]);
 };
