61d1e0bce021a9ac89401463ea954f13bddd71a1
Jophine Antony <jophine.antony@sungardas.com>
2018-07-23 21:26:42 +0530

    MA-3620: Addressed eslint issues

--- src/index_trigger_cloudchecker_sf.js
+++ src/index_trigger_cloudchecker_sf.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
 
 exports.handler = function(event, context, callback) {
     /*************
@@ -12,17 +12,17 @@ exports.handler = function(event, context, callback) {
     **************/
 
     // Initializations
-    var stepfunctions = new AWS.StepFunctions();
-    var cloudchecker_sf_arn = event.cloudCheckerSfArn;
-    var acc_id = event.dbAwsAccount.awsid;
-    var acc_name = event.dbAwsAccount.name;
-    var execution_name = `CloudCheck-For-${acc_name}-${Date.now()}`;
-    var execution_input = {
+    let stepfunctions = new AWS.StepFunctions();
+    let cloudchecker_sf_arn = event.cloudCheckerSfArn;
+    let acc_id = event.dbAwsAccount.awsid;
+    let acc_name = event.dbAwsAccount.name;
+    let execution_name = `CloudCheck-For-${acc_name}-${Date.now()}`;
+    let execution_input = {
         accountTag: event.accountTag,
         customerAccountId: acc_id,
         customerAccountName: acc_name
     }
-    var params = {
+    let params = {
         stateMachineArn: cldchk_stpfn_arn,
         name: execution_name,
         input: JSON.stringify(execution_input)
