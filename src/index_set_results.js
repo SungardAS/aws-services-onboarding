acfc8d572f05786b5d47a9f0d05eb80f5757c222
Mayank  Bharadwaj <mayank.bharadwaj@sungardas.com>
2018-08-03 15:02:09 +0530

    MA-3750/Modified the result set file to modify null condition

--- src/index_set_results.js
+++ src/index_set_results.js
@@ -1,12 +1,10 @@
 
 exports.handler = (event, context, callback) => {
   // we've got all outputs of the parallel actions, so merge results to the first action's output
-  if(event[0].cloudtrail.result != null){
-    var cloudtrailResult = JSON.parse(event[0].cloudtrail.result.body);
-    event[0].final_result.cloudtrail[event[0].cloudtrail.body.region] = cloudtrailResult.result;
-  }
-  else
-    event[0].final_result.cloudtrail[event[0].cloudtrail.body.region] = false;
+  var cloudtrailResult= false;
+  if(event[0].cloudtrail.result != null)
+    cloudtrailResult = JSON.parse(event[0].cloudtrail.result.body.result);
+  event[0].final_result.cloudtrail[event[0].cloudtrail.body.region] = cloudtrailResult;
   var awsconfigResult = JSON.parse(event[1].awsconfig.result.body);
   event[0].final_result.awsconfig[event[0].awsconfig.body.region] = awsconfigResult.result;
   var awseventsResult = JSON.stringify(event[0].awsevents.result);
