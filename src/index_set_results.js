a4f7a968c4d2cee63f85a6eae865e9221aa1ca27
mayankbharadwaj <mak.ideal@gmail.com>
2018-08-07 16:07:54 +0530

    MA-3785/bug fix for result set

--- src/index_set_results.js
+++ src/index_set_results.js
@@ -2,8 +2,10 @@
 exports.handler = (event, context, callback) => {
   // we've got all outputs of the parallel actions, so merge results to the first action's output
   var cloudtrailResult= false;
-  if(event[0].cloudtrail.result != null)
-    cloudtrailResult = JSON.parse(event[0].cloudtrail.result.body.result);
+  if(event[0].cloudtrail.result != null){
+    var cloudtrailResultBody = JSON.parse(event[0].cloudtrail.result.body);
+    cloudtrailResult = cloudtrailResultBody.result;
+  }
   event[0].final_result.cloudtrail[event[0].cloudtrail.body.region] = cloudtrailResult;
   var awsconfigResult = JSON.parse(event[1].awsconfig.result.body);
   event[0].final_result.awsconfig[event[0].awsconfig.body.region] = awsconfigResult.result;
