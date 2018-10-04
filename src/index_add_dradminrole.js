ca0546d992b23e1960e1e4eea8c543a58a7c5956
Jophine Antony <jophine.antony@sungardas.com>
2018-10-04 20:51:50 +0530

    CBR-2322: Modified add dradmin role lmabda

--- src/index_add_dradminrole.js
+++ src/index_add_dradminrole.js
@@ -4,17 +4,14 @@ const AWS = require('aws-sdk');
 const McawsModels = require('./models/mcawsModels.js');
 
 exports.handler = function(event, context, callback) {
+  const dbIamRoles = event.dbIamRoles;
   const dbAwsAccount = event.dbAwsAccount;
-  const dradminRole = event.dradminRole;
   const fullAdminRoleName = 'FullAdmin';
+  const dradminRole = event.dradminRole;
   const encryptedBuf = new Buffer(process.env.DB_PASSWORD, 'base64');
   const cipherText = { CiphertextBlob: encryptedBuf };
   const kms = new AWS.KMS({ region: process.env.KMS_REGION });
-
-  if(dbAwsAccount.account_type.toLowerCase() != 'craws') {
-    console.log("Account type is not craws. Hence skipping.");
-    callback(null, event);
-  }
+  let accDetails = null;
   kms.decrypt(cipherText, (err, passwd) => {
     if (err) {
       console.log('Decrypt error:', err);
@@ -27,94 +24,126 @@ exports.handler = function(event, context, callback) {
       process.env.DB_HOST,
       'msaws'
     );
-    mcawsDbObj.Role(roleResp => {
-      roleResp.sync().then(() =>
-        roleResp.findOne({
-          where: dradminRole
-        }).then(roleData => {
-          if(roleData) {
-            return roleData;
-          } else {
-            console.log("dradmin role name provided does not exists");
-            throw new Error("dradmin role name error");
-          }
-        }).then(roleDataResp => {
-          mcawsDbObj.AwsAccount(accResp => {
-            accResp.sync().then(() =>
-              accResp.findOne({
-                where: dbAwsAccount
-              }).then(accountData => {
-                if(accountData) {
-                  console.log("Account entry is there.");
-                  return accountData;
-                } else {
-                  console.log("Account entry is not there.");
-                  throw new Error("Account entry missing");
-                }
-              }).then(accData => {
-                mcawsDbObj.AwsAccountAdminRolesRoleAdminAwsAccounts(accRole => {
-                  accRole.findOne({
-                    where: {
-                      awsaccount_adminroles: accData.id,
-                      role_adminawsaccounts: roleDataResp.id
-                    }
-                  }).then(accRoleResp => {
-                    if(accRoleResp) {
-                      console.log("Role already added to awsaccount");
-                      return accRoleResp;
-                    } else return accRoleResp.create({
-                      awsaccount_adminroles: accData.id,
-                      role_adminawsaccounts: roleDataResp.id
-                    });
-                  }).then(accRoleData => {
-                    console.log("Role added to awsaccount");
-                    console.log(accRoleData);
-                    return accRoleData;
-                  }).then(accRoleDataResp => {
-                    mcawsDbObj.AwsIamRole(iamResp => {
-                      iamResp.findOne({
-                        where: {
-                          account: accData.id,
-                          name: fullAdminRoleName
-                        }
-                      }).then(iamData => {
-                        if(iamData) {
-                          console.log("IAM role exists");
-                          return iamData;
-                        } else {
-                          console.log("IAM role is missing");
-                          throw new Error("IAM role is missing");
-                        }
-                      }).then(iamDataResp => {
-                        mcawsDbObj.AwsIamRoleRolesRoleAwsIamRoles(rolIamRol => {
-                          rolIamRol.findOne({
-                            where: {
-                              awsiamrole_roles: iamDataResp.id,
-                              role_awsiamroles: roleDataResp.id
-                            }
-                          }).then(rolIamRolResp => {
-                            if(rolIamRolResp) {
-                              console.log("Role already added to IAM role");
-                              return rolIamRolResp;
-                            } else return rolIamRolResp.create({
-                              awsiamrole_roles: iamDataResp.id,
-                              role_awsiamroles: roleDataResp.id
-                            });
-                          }).then(rolIamRolData => {
-                            console.log("Role added to IAM role");
-                            console.log(rolIamRolData);
+    mcawsDbObj.AwsAccount(accResp => {
+      accResp.sync().then(() =>
+        accResp.findOne({
+             where: dbAwsAccount
+        }).then(accountData =>{
+             if(accountData) {
+               console.log("Account entry is already there.");
+               console.log(accountData);
+                return accountData;
+             }
+             else return accResp.create(dbAwsAccount);
+         })
+          .then(accData => {
+            accDetails = JSON.parse(JSON.stringify(accData));
+            console.log(`accDetails: ${JSON.stringify(accDetails)}`);
+            console.log(`accData: ${JSON.stringify(accData)}`);
+            for (let idx = 0; idx < dbIamRoles.length; idx++) {
+              dbIamRoles[idx].account = accData.dataValues.id;
+              mcawsDbObj.AwsIamRole(iamResp =>
+                iamResp.findOne({
+                   where: {arn: dbIamRoles[idx].arn}
+                })
+                .then(resultData =>{
+                  if(resultData) {
+                     console.log("IAM Role entry is already there.");
+                     return resultData;
+                  }
+                  else return iamResp.create(dbIamRoles[idx]);
+                })
+                .then(roleData => {
+                  console.log('role updation Done :)');
+                  console.log(roleData);
+                })
+                .catch(errRole => console.log(errRole))
+              );
+            }
+          })
+          .then(() => {
+            if(dbAwsAccount.account_type.toLowerCase() != 'craws') {
+              console.log("Account type is not craws. Hence skipping.");
+            } else {
+              mcawsDbObj.Role(roleResp => {
+                roleResp.findOne({
+                  where: dradminRole
+                })
+                .then(roleData => {
+                  if(roleData) {
+                    return roleData;
+                  } else {
+                    console.log("dradmin role name provided does not exists");
+                    throw new Error("dradmin role name error");
+                  }
+                })
+                .then(roleDataResp => {
+                  mcawsDbObj.AwsAccountAdminRolesRoleAdminAwsAccounts(accRole =>
+                    accRole.findOne({
+                      where: {
+                        awsaccount_adminroles: accDetails.id,
+                        role_adminawsaccounts: roleDataResp.dataValues.id
+                      }
+                    })
+                    .then(accRoleResp => {
+                      if(accRoleResp) {
+                        console.log("Role already added to awsaccount");
+                        return accRoleResp;
+                      } else return accRole.create({
+                        awsaccount_adminroles: accDetails.id,
+                        role_adminawsaccounts: roleDataResp.dataValues.id
+                      })
+                    })
+                    .then(accRoleData => {
+                      console.log("Role added to awsaccount");
+                      console.log(accRoleData);
+                    })
+                  );
+                  mcawsDbObj.AwsIamRole(iamRole =>
+                    iamRole.findOne({
+                      where: {
+                        account: accDetails.id,
+                        name: fullAdminRoleName
+                      }
+                    })
+                    .then(iamRoleResp => {
+                      if(iamRoleResp) {
+                        return iamRoleResp;
+                      } else {
+                        console.log("Target role does not exists");
+                        throw new Error("Target role does not exists");
+                      }
+                    })
+                    .then(iamRoleData =>
+                      mcawsDbObj.AwsIamRoleRolesRoleAwsIamRoles(iamRoleRole =>
+                        iamRoleRole.findOne({
+                          where: {
+                            awsiamrole_roles: iamRoleData.dataValues.id,
+                            role_awsiamroles: roleDataResp.dataValues.id
+                          }
+                        })
+                        .then(iamRoleRoleResp => {
+                          if(iamRoleRoleResp) {
+                            console.log("Role already added to iam roles");
+                            return iamRoleRoleResp;
+                          } else return iamRoleRole.create({
+                            awsiamrole_roles: iamRoleData.dataValues.id,
+                            role_awsiamroles: roleDataResp.dataValues.id
                           })
-                          .catch(err => console.log(err))
-                          .finally(() => mcawsDbObj.CloseConnection())
-                        });
-                      });
-                    });
-                  });
-                });
+                        })
+                        .then(iamRoleRoleData => {
+                          console.log("Role added to iam role")
+                          console.log(iamRoleRoleData);
+                        })
+                        .then(() => mcawsDbObj.CloseConnection())
+                      )
+                    )
+                  );
+                })
               });
-            );
-          });
-        });
+            }
+          })
+        .catch(errAcc => console.log(errAcc))
       );
     });
   });
